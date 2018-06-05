12de488baffcf44fbd79ac361572a54288efd635
Benjamin Maier <maier.bn@gmail.com>
2018-06-05 14:41:06 +0200

    Add pythonpath and path of scripts in test cases run script

--- testing/system_testing/run.sh
+++ testing/system_testing/run.sh
@@ -2,6 +2,10 @@
 
 workdir=$(pwd)
 
+# set path and pythonpath to include scripts
+export PYTHONPATH=$PYTHONPATH:$(pwd)/../../scripts/
+export PATH=$PATH:$(pwd)/../../scripts/
+
 # run all tests, this includes compiling the documents
 cd $workdir/tests
 ./run.sh

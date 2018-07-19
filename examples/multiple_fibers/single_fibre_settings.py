b2067a283625ca01b5b6da479d57461a36241838
Benjamin Maier <maier.bn@gmail.com>
2018-07-19 22:38:35 +0200

    Change single_fibre from Godunov to Strang

--- examples/multiple_fibers/single_fibre_settings.py
+++ examples/multiple_fibers/single_fibre_settings.py
@@ -1,6 +1,7 @@
 # Electrophysiology debug
 #
-#  parameters: <fibre no>
+#  parameters: <fibre no> [<subset_length>]
+#  subset_length is the number of nodes to use from the fibre mesh
 
 endTime = 10.0
 
@@ -15,28 +16,46 @@ import sys
 PMax = 7.3              # maximum stress [N/cm^2]
 Conductivity = 3.828    # sigma, conductivity [mS/cm]
 Am = 500.0              # surface area to volume ratio [cm^-1]
-Cm = 0.58           # membrane capacitance [uF/cm^2]
-stimulation_frequency = 10.0 # stimulations per ms
+Cm = 0.58               # membrane capacitance [uF/cm^2]
+innervation_zone_width = 1.  # cm
+innervation_zone_width = 0.  # cm
+  
 
+# timing parameters
+stimulation_frequency = 10.0      # stimulations per ms
+dt_1D = 1e-5                      # timestep width of diffusion
+dt_0D = 5e-5                      # timestep width of ODEs
+dt_3D = 1e-1                      # overall timestep width of splitting
+output_timestep = 1e-1            # timestep for output files
+
+# input files
 #fibre_file = "../input/laplace3d_structured_quadratic"
 fibre_file = "../input/laplace3d_structured_linear"
 fibre_distribution_file = "../input/MU_fibre_distribution_3780.txt"
 #firing_times_file = "../input/MU_firing_times_real.txt"
 firing_times_file = "../input/MU_firing_times_immediately.txt"
 
-print("prefactor: ",Conductivity/(Am*Cm))
-print("numpy path: ",np.__path__)
+#print("prefactor: ",Conductivity/(Am*Cm))
+#print("numpy path: ",np.__path__)
 
+# parse fibre number as first command line parameter
 fibre_no = 0
 if len(sys.argv) > 0:
   try:
     fibre_no = int(sys.argv[0])
     print("fibre_no: {}".format(fibre_no))
     
-    
-    
   except:
     print("could not parse fibre_no");
+
+subset_length = None
+if len(sys.argv) > 1:
+  try:
+    subset_length = int(sys.argv[1])
+    print("subset_length: {}".format(subset_length))
+  except:
+    print("could not parse subset_length");
+
 
 def getMotorUnitNo(fibre_no):
   return int(fibre_distribution[fibre_no % len(fibre_distribution)]-1)
@@ -57,7 +76,6 @@ def setParameters(n_nodes, time_step_no, current_time, parameters, fibre_no):
   fibre_gets_stimulated = fibreGetsStimulated(fibre_no, stimulation_frequency, current_time)
   
   # determine nodes to stimulate (center node, left and right neighbour)
-  innervation_zone_width = 1.  # cm
   innervation_zone_width_n_nodes = innervation_zone_width*100  # 100 nodes per cm
   innervation_node = int(n_nodes / 2) + np.random.randint(-innervation_zone_width_n_nodes/2,innervation_zone_width_n_nodes/2+1)
   nodes_to_stimulate = [innervation_node]
@@ -75,8 +93,8 @@ def setParameters(n_nodes, time_step_no, current_time, parameters, fibre_no):
   for node_no in nodes_to_stimulate:
     parameters[node_no] = stimulation_current
 
-  print("       setParameters at timestep {}, t={}, n_nodes={}, IStim={}, fibre no {}, MU {}, stimulated: {}".\
-    format(time_step_no, current_time, n_nodes, parameters[0], fibre_no, getMotorUnitNo(fibre_no), fibre_gets_stimulated))
+  print("       setParameters at timestep {}, t={}, n_nodes={}, fibre no {}, MU {}, stimulated: {}".\
+    format(time_step_no, current_time, n_nodes, fibre_no, getMotorUnitNo(fibre_no), fibre_gets_stimulated))
     
  
   print("       set stimulation for nodes {}".format(nodes_to_stimulate))
@@ -85,27 +103,6 @@ def setParameters(n_nodes, time_step_no, current_time, parameters, fibre_no):
     
 fig = plt.figure(1)
 #plt.ion()
-
-def debug(n_nodes, time_step_no, current_time, states, intermediates, null):
-  print("time step {}, t={}, n_nodes: {}, n states: {}".format(time_step_no, current_time, n_nodes, len(states)))
-  
-  center_node = int(n_nodes / 2)
-  
-  states_of_center_node = [states[n_nodes*i + center_node] for i in range(57)]
-  intermediates_of_center_node = [intermediates[n_nodes*i + center_node] for i in range(1)]
-  
-  print("states_of_center_node:",states_of_center_node)
-  print("intermediates_of_center_node:",intermediates_of_center_node)
-  
-  
-  node0 = int(n_nodes / 4.)
-  
-  states_of_node0 = [states[n_nodes*i + node0] for i in range(57)]
-  intermediates_of_node0 = [intermediates[n_nodes*i + node0] for i in range(1)]
-  
-  print("states_of_node0:",states_of_node0)
-  print("intermediates_of_node0:",intermediates_of_node0)
-  wait = input("PRESS ENTER TO CONTINUE.")
 
 def handleResult(n_nodes, time_step_no, current_time, states, intermediates):
   #print "handleResult: time step {}, t={}, n_nodes: {}, n entries states: {}".format(time_step_no, current_time, n_nodes, len(states))
@@ -150,16 +147,16 @@ def get_instance_config(i):
   
   bc = {0: -80.25667, -1: -80.25667}
   instance_config = {
-    "GodunovSplitting": {
+    "StrangSplitting": {
       #"numberTimeSteps": 1,
-      "timeStepWidth": 1e-1,
+      "timeStepWidth": dt_3D,  # 1e-1
       "endTime": endTime,
       "outputData1": False,
       "outputData2": True,
 
       "Term1": {      # CellML
         "Heun" : {
-          "timeStepWidth": 5e-5,  # 5e-5
+          "timeStepWidth": dt_0D,  # 5e-5
           #"initialValues": [],
           "timeStepOutputInterval": 1e4,
           
@@ -170,7 +167,7 @@ def get_instance_config(i):
             "forceRecompileRhs": False,
             #"statesInitialValues": [],
             "setParametersFunction": setParameters,
-            "setParametersCallInterval": 1e3,          # setParameters should be called every 0.1, 5e-5 * 1e3 = 5e-2 = 0.05
+            "setParametersCallInterval":  1./stimulation_frequency/dt_0D,     # 1e3     # setParameters should be called every 0.1, 5e-5 * 1e3 = 5e-2 = 0.05
             "setParametersFunctionAdditionalParameter": i,
             
             #"handleResultFunction": debug,
@@ -191,7 +188,7 @@ def get_instance_config(i):
         "Heun" : {
           #"initialValues": [2,2,4,5,2,2],
           #"numberTimeSteps": 1,
-          "timeStepWidth": 1e-5,  # 1e-5
+          "timeStepWidth": dt_1D,  # 1e-5
           "timeStepOutputInterval": 1e4,
           "FiniteElementMethod" : {
             "relativeTolerance": 1e-10,
@@ -200,11 +197,9 @@ def get_instance_config(i):
             "DirichletBoundaryCondition": bc,
           },
           "OutputWriter" : [
-            {"format": "Paraview", "outputInterval": 1e4, "filename": "out/bin_"+str(i), "binaryOutput": True, "fixedFormat": False},
-            {"format": "Paraview", "outputInterval": 1e4, "filename": "out/txt_"+str(i), "binaryOutput": False, "fixedFormat": False},
-            #{"format": "ExFile", "filename": "out/fibre_"+str(i), "outputInterval": 1e4, "sphereSize": "0.02*0.02*0.02"},
-            {"format": "PythonFile", "filename": "out/fibre_bin_"+str(i), "outputInterval": 1e4, "binary":True, "onlyNodalValues":True},
-            {"format": "PythonFile", "filename": "out/fibre_"+str(i), "outputInterval": 1e4, "binary":False, "onlyNodalValues":True},
+            {"format": "Paraview", "outputInterval": 1./dt_1D*output_timestep, "filename": "out/fibre_"+str(i), "binaryOutput": True, "fixedFormat": False},
+            #{"format": "ExFile", "filename": "out/fibre_"+str(i), "outputInterval": 1./dt_1D*output_timestep, "sphereSize": "0.02*0.02*0.02"},
+            {"format": "PythonFile", "filename": "out/fibre_"+str(i), "outputInterval": 1./dt_1D*output_timestep, "binary":True, "onlyNodalValues":True},
           ]
         },
       },
@@ -225,6 +220,19 @@ print("nInstances: {}".format(nInstances))
 #nInstances = 1
     
 for i,streamline in enumerate(streamlines):
+  
+  # extract only part of streamline according to given subset_length
+  if subset_length is not None:
+    streamline = streamline[1:min(subset_length,len(streamline))]
+  
+    if i == fibre_no:
+      print("element lengths: ")
+      distances = []
+      for j in range(len(streamline)-1):
+        distances.append(np.linalg.norm(np.array(streamline[j]) - np.array(streamline[j+1])))
+      print(distances)
+  
+  # define mesh
   meshes["MeshFibre{}".format(i)] = {
     "nElements": len(streamline)-1,
     "nodePositions": streamline

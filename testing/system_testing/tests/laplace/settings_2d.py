f0561612ff8a3bf090b4bd7c65bcd31f231c7655
Benjamin Maier <maier.bn@gmail.com>
2018-07-23 23:02:12 +0200

    Update system_testing latex documents

--- testing/system_testing/tests/laplace/settings_2d.py
+++ testing/system_testing/tests/laplace/settings_2d.py
@@ -108,6 +108,7 @@ config = {
     "nodePositions": node_positions,
     "elements": elements,
     "physicalExtent": physicalExtent,
+    "prefactor": 0.6,
     "DirichletBoundaryCondition": bc,
     "relativeTolerance": 1e-15,
     "maxIterations": 500000,

38235bb78bb529ac94f9195733651f7f99c43447
Benjamin Maier <maierbn@gmail.com>
2018-09-17 13:44:18 +0200

    Adjust multiple_fibers test for neon

--- testing/system_testing/tests/multiple_fibers/run_tests.sh
+++ testing/system_testing/tests/multiple_fibers/run_tests.sh
@@ -7,8 +7,8 @@ variant="debug"
 variant="release"
 
 # copy fibre results from fibers system test to this folder
-cd input
-. copy.sh
+#cd input
+#. copy.sh
 cd $workdir
 
 mkdir -p build_${variant}
@@ -22,6 +22,6 @@ export OMP_NUM_THREADS=1
 
 # arguments: <fibre no> <cellml_file> <end_time>
 #./single_fibre ../single_fibre_settings.py 1 "../input/hodgkin_huxley_1952.c" 10
-./multiple_fibers ../multiple_fibers_settings.py
+mpirun -n $1 ./multiple_fibers ../multiple_fibers_settings.py
 
 cd $workdir

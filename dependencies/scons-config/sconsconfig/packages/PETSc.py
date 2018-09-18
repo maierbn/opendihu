b3cdb70325e16031814de34b7ab53249e1e40404
Benjamin Maier <maier.bn@gmail.com>
2018-09-18 15:24:06 +0200

    do not run petsc on hazel hen login node

--- dependencies/scons-config/sconsconfig/packages/PETSc.py
+++ dependencies/scons-config/sconsconfig/packages/PETSc.py
@@ -67,6 +67,9 @@ class PETSc(Package):
             self.extra_libs = ["sci_gnu_71_mpi_mp"]
           print("{} environment detected, using \"{}\" for Petsc".format(os.environ.get("PE_ENV"), self.libs[0]))
         
+          # on hazel hen login node do not run MPI test program because this is not possible (only compile)
+          self.run = False
+          
         self.check_text = petsc_text
         self.static = False
         #self.set_rpath = False

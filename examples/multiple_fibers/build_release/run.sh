3100d0a8533a104ac3991de17d89aa37a803ae3e
emamynt <nehzat.emamy@ipvs.uni-stuttgart.de>
2018-11-06 11:41:09 +0100

    run script for intel compiler

--- examples/multiple_fibers/build_release/run.sh
+++ examples/multiple_fibers/build_release/run.sh
@@ -1,31 +1,31 @@
 # reference
 for i in `seq 1 5`; do
-  aprun -n 48 -N 24 ./multiple_fibers_gnu ../settings.py 4 gnu_reference       # 4 = 4 processes per fiber
+  aprun -n 48 -N 24 ./multiple_fibers_intel ../settings.py 4 intel_reference       # 4 = 4 processes per fiber
 done
 
 # packed turbo off
 for i in `seq 1 5`; do
-  aprun -n 48 -N 24 --p-state 2500000 ./multiple_fibers_gnu ../settings.py 4 gnu_turbo_off       # 4 = 4 processes per fiber
+  aprun -n 48 -N 24 --p-state 2500000 ./multiple_fibers_intel ../settings.py 4 intel_turbo_off       # 4 = 4 processes per fiber
 done
 
 # packed slow
 for i in `seq 1 5`; do
-  aprun -n 48 -N 24 --p-state 1250000 ./multiple_fibers_gnu ../settings.py 4 gnu_slow       # 4 = 4 processes per fiber
+  aprun -n 48 -N 24 --p-state 1250000 ./multiple_fibers_intel ../settings.py 4 intel_slow       # 4 = 4 processes per fiber
 done
 
 # non-packed turbo off
 for i in `seq 1 5`; do
-  aprun -n 48 -N 24 -S 12 --p-state 2500000 ./multiple_fibers_gnu ../settings.py 4 gnu_non-packed_turbo_off       # 4 = 4 processes per fiber
+  aprun -n 48 -N 24 -S 12 --p-state 2500000 ./multiple_fibers_intel ../settings.py 4 intel_non-packed_turbo_off       # 4 = 4 processes per fiber
 done
 
 # non-packed slow
 for i in `seq 1 5`; do
-  aprun -n 48 -N 24 -S 12 --p-state 1250000 ./multiple_fibers_gnu ../settings.py 4 gnu_non-packed_slow       # 4 = 4 processes per fiber
+  aprun -n 48 -N 24 -S 12 --p-state 1250000 ./multiple_fibers_intel ../settings.py 4 intel_non-packed_slow       # 4 = 4 processes per fiber
 done
 
 # hyperthreads
 for i in `seq 1 5`; do
-  aprun -n 48 -N 48 -S 24 -j 2 --p-state 1250000 ./multiple_fibers_gnu ../settings.py 4 gnu_hyperthreads       # 4 = 4 processes per fiber
+  aprun -n 48 -N 48 -S 24 -j 2 --p-state 1250000 ./multiple_fibers_intel ../settings.py 4 intel_hyperthreads       # 4 = 4 processes per fiber
 done
 
 # hugepages
@@ -33,7 +33,7 @@ module unload craype-hugepages16M
 module load craype-hugepages64M
 
 for i in `seq 1 5`; do
-  aprun -n 48 -N 24 ./multiple_fibers_gnu ../settings.py 4 gnu_hugepages       # 4 = 4 processes per fiber
+  aprun -n 48 -N 24 ./multiple_fibers_intel ../settings.py 4 intel_hugepages       # 4 = 4 processes per fiber
 done
 
 module unload craype-hugepages64M

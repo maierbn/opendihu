c798e263915819b879be5982d8f8e430e653d320
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2018-11-06 10:07:19 +0100

    run.sh

--- examples/multiple_fibers/build_release/run.sh
+++ examples/multiple_fibers/build_release/run.sh
@@ -1,3 +1,3 @@
 for i in `seq 1 5`; do
-  aprun -n 24 ./multiple_fibers_gnu ../settings.py 4    # 4 = 4 processes per fiber
+  aprun -n 48 -N 24 ./multiple_fibers_gnu ../settings.py 4 gnu_reference       # 4 = 4 processes per fiber
 done

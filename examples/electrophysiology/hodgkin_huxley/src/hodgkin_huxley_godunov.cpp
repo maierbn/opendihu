4a2007d003d9c9ce3cb7541237c055d9770af768
emamy <Nehzat.Emamy@ipvs.uni-stuttgart.de>
2018-11-12 09:31:24 +0100

    H-H with explicit timestepping for test

--- examples/electrophysiology/hodgkin_huxley/src/hodgkin_huxley_godunov.cpp
+++ examples/electrophysiology/hodgkin_huxley/src/hodgkin_huxley_godunov.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     TimeSteppingScheme::ExplicitEuler<
       CellmlAdapter<4>
     >,
-    TimeSteppingScheme::ImplicitEuler<
+    TimeSteppingScheme::ExplicitEuler<
       SpatialDiscretization::FiniteElementMethod<
         Mesh::StructuredRegularFixedOfDimension<1>,
         BasisFunction::LagrangeOfOrder<1>,

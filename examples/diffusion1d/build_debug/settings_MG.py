d361372a817ff4f21842e3cafb600bffc28430bb
emamy <Nehzat.Emamy@ipvs.uni-stuttgart.de>
2019-01-17 10:12:32 +0100

    changes for MG

--- examples/diffusion1d/build_debug/settings_MG.py
+++ examples/diffusion1d/build_debug/settings_MG.py
@@ -4,15 +4,11 @@ dt=0.01
 end_time=1;
 
 config = {
-  "multigrid_Vcycle": {
-  "timeStepWidth": 1e-1
-    "endTime": end_time,
-    "durationLogKey": "duration_total",
-    "timeStepOutputInterval": 10,
-  "Term1": {
   "ExplicitEuler" : {
     "initialValues": [2,2,4,5,2,2],
     "timeStepWidth":dt/2,
+    "multigrid_Vcycle": {
+    "Term1": {
     "FiniteElementMethod" : {
       "nElements": n,
       "physicalExtent": 4.0,
@@ -24,11 +20,8 @@ config = {
       {"format": "PythonFile", "filename": "out/diffusion1d", "outputInterval": 10, "binary":False}
     ]
   },
-  },
-  "Term2": {  
-  "ExplicitEuler" : {
-    "initialValues": [2,2,4,5,2,2],
-    "timeStepWidth":dt/2,
+  },  
+    "Term2": {  
     "FiniteElementMethod" : {
       "nElements": n,
       "physicalExtent": 4.0,

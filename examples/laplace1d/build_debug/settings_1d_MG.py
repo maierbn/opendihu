cc9466c762c7b62caa9f27cdaa5bb59f44f0d4e8
Tobias Walter <walterto@ipvs.uni-stuttgart.de>
2019-02-07 09:49:43 +0100

    settings for multigrid numCycles

--- examples/laplace1d/build_debug/settings_1d_MG.py
+++ examples/laplace1d/build_debug/settings_1d_MG.py
@@ -12,30 +12,32 @@ bc[n] = 0.0
 config = {
   "disablePrinting": False,
   "disableMatrixPrinting": False,
-  "numCycles" : 100,
-  "Term1":{
-  "FiniteElementMethod" : {
-    "nElements": n,
-    "physicalExtent": 4.0,
-    "dirichletBoundaryConditions": bc,
-    "relativeTolerance": 1e-15,
-    "OutputWriter" : [
-      #{"format": "Paraview", "interval": 1, "filename": "out/p1", "binaryOutput": "false", "fixedFormat": False},
-      {"format": "PythonFile", "interval": 1, "filename": "out/p1"}
-    ]
-  }
-  },
-  "Term2":{
-  "FiniteElementMethod" : {
-    "nElements": n,
-    "physicalExtent": 4.0,
-    "dirichletBoundaryConditions": bc,
-    "relativeTolerance": 1e-15,
-    "OutputWriter" : [
-
-      #{"format": "Paraview", "interval": 1, "filename": "out/p2", "binaryOutput": "false", "fixedFormat": False},
-      {"format": "PythonFile", "interval": 1, "filename": "out/p2"}
-    ]
-  }
-  },
+  
+  "Multigrid" : {
+      "numCycles" : 100,
+      "Term1":{
+          "FiniteElementMethod" : {
+              "nElements": n,
+              "physicalExtent": 4.0,
+              "dirichletBoundaryConditions": bc,
+              "relativeTolerance": 1e-15,
+              "OutputWriter" : [
+                  #{"format": "Paraview", "interval": 1, "filename": "out/p1", "binaryOutput": "false", "fixedFormat": False},
+                  {"format": "PythonFile", "interval": 1, "filename": "out/p1"}
+                  ]
+              }
+        },
+        "Term2":{
+            "FiniteElementMethod" : {
+                "nElements": n,
+                "physicalExtent": 4.0,
+                "dirichletBoundaryConditions": bc,
+                "relativeTolerance": 1e-15,
+                "OutputWriter" : [
+                #{"format": "Paraview", "interval": 1, "filename": "out/p2", "binaryOutput": "false", "fixedFormat": False},
+                {"format": "PythonFile", "interval": 1, "filename": "out/p2"}
+                ]
+                }
+        },
+    },
 }

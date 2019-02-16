1878c481bc5ebcdb94c1aaa30beeb2f0393d5755
Benjamin Maier <maierbn@gmail.com>
2019-02-16 21:20:42 +0100

    Disable checkpoint logs

--- examples/parallel_fiber_estimation/settings.py
+++ examples/parallel_fiber_estimation/settings.py
@@ -25,14 +25,14 @@ config = {
   },
   "ParallelFiberEstimation" : {
     "stlFilename": "../../../testing/system_testing/tests/fibers/meshes/biceps_full.stl",
-    "resultFilename": "fibers.bin",
+    "resultFilename": "15x15fibers.bin",
     "bottomZClip":  72.0,   # 82 (72), bottom z value of the muscle volume  
     "topZClip": 220.0,      # 250 (220), top z value of the muscle volume
     "nElementsXPerSubdomain": 4,  # number of elements in x and y-direction per subdomain
     "nElementsZPerSubdomain": 50,  # number of elements in z-direction per subdomain
-    "nFineGridFibers": 37,     # number of additional fine fibers that are interpolated between the main "key" fibers, the key fibers are traced
+    "nFineGridFibers": 55,     # number of additional fine fibers that are interpolated between the main "key" fibers, the key fibers are traced
     "useGradientField": False,    # set to False
-    "maxLevel": 1,          # maximum level (1=8 processes, 2=64 processes)
+    "maxLevel": 2,          # maximum level (1=8 processes, 2=64 processes)
     "lineStepWidth":  0.1,  # line width for tracing of fibers
     "nNodesPerFiber": (220.-72.) / 0.1,   # number of nodes in each final fiber
     "improveMesh": True,     # smooth the 2D meshes, required for bigger meshes or larger amount of ranks

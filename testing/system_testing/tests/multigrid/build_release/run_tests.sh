930252d2968f842139dd5d12e1d3436585ffd676
Tobias <st143615@stud.uni-stuttgart.de>
2019-04-03 13:40:28 +0200

    cuboid multigrid neon tests

--- testing/system_testing/tests/multigrid/build_release/run_tests.sh
+++ testing/system_testing/tests/multigrid/build_release/run_tests.sh
@@ -11,18 +11,38 @@ mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 400 solver_scaling gmres none   
 mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 400 solver_scaling preonly gamg    | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
 mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 400 solver_scaling richardson pchypre      | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
 
-mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 800 solver_scaling gmres none      | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
-mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 800 solver_scaling preonly gamg    | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
-mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 800 solver_scaling richardson pchypre      | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
+mpiexec -n 6 ./cuboid ../cuboid_settings.py 6 1 600 solver_scaling gmres none      | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
+mpiexec -n 6 ./cuboid ../cuboid_settings.py 6 1 600 solver_scaling preonly gamg    | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
+mpiexec -n 6 ./cuboid ../cuboid_settings.py 6 1 600 solver_scaling richardson pchypre      | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
 
-#mpiexec -n 16 ./cuboid ../cuboid_settings.py 16 1 1600 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
-#mpiexec -n 16 ./cuboid ../cuboid_settings.py 16 1 1600 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
-#mpiexec -n 16 ./cuboid ../cuboid_settings.py 16 1 1600 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+mpiexec -n 12 ./cuboid ../cuboid_settings.py 12 1 1200 solver_scaling gmres none      | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
+mpiexec -n 12 ./cuboid ../cuboid_settings.py 12 1 1200 solver_scaling preonly gamg    | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
+mpiexec -n 12 ./cuboid ../cuboid_settings.py 12 1 1200 solver_scaling richardson pchypre      | tee -a out_solver_scaling_1.txt 2>&1 # 1 nodes
 
-#mpiexec -n 32 ./cuboid ../cuboid_settings.py 32 1 3200 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
-#mpiexec -n 32 ./cuboid ../cuboid_settings.py 32 1 3200 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
-#mpiexec -n 32 ./cuboid ../cuboid_settings.py 32 1 3200 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 1800 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 1800 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 1800 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+
+#mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 3000 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 3000 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 3000 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+
+#mpiexec -n 36 ./cuboid ../cuboid_settings.py 64 1 6400 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 36 ./cuboid ../cuboid_settings.py 64 1 6400 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 36 ./cuboid ../cuboid_settings.py 64 1 6400 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+
+#mpiexec -n 46 ./cuboid ../cuboid_settings.py 46 1 4600 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 46 ./cuboid ../cuboid_settings.py 46 1 4600 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 46 ./cuboid ../cuboid_settings.py 46 1 4600 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+
+#mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 5400 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 5400 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 5400 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
 
 #mpiexec -n 64 ./cuboid ../cuboid_settings.py 64 1 6400 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
 #mpiexec -n 64 ./cuboid ../cuboid_settings.py 64 1 6400 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
 #mpiexec -n 64 ./cuboid ../cuboid_settings.py 64 1 6400 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+
+#mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 7200 solver_scaling gmres none      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 7200 solver_scaling preonly gamg    | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes
+#mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 7200 solver_scaling richardson pchypre      | tee -a out_solver_scaling_2.txt 2>&1 # 2 nodes

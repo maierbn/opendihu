96b419639d77c06faf867dce0b4f88ddc613a74a
emamy <Nehzat.Emamy@ipvs.uni-stuttgart.de>
2019-04-12 19:19:27 +0200

    setting unit test for Diffusion1D with POD

--- testing/unit_testing/src/1_rank/diffusion.cpp
+++ testing/unit_testing/src/1_rank/diffusion.cpp
@@ -153,8 +153,80 @@ config = {
 
   problem.run();
 
-  std::string referenceOutput = "{\"meshType\": \"StructuredRegularFixed\", \"dimension\": 1, \"nElementsGlobal\": [5], \"nElementsLocal\": [5], \"beginNodeGlobalNatural\": [0], \"hasFullNumberOfNodes\": [true], \"basisFunction\": \"Lagrange\", \"basisOrder\": 1, \"onlyNodalValues\": true, \"nRanks\": 1, \"ownRankNo\": 0, \"data\": [{\"name\": \"geometry\", \"components\": [{\"name\": \"x\", \"values\": [0.0, 0.8, 1.6, 2.4000000000000004, 3.2, 4.0]}, {\"name\": \"y\", \"values\": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}, {\"name\": \"z\", \"values\": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}]}, {\"name\": \"solution\", \"components\": [{\"name\": \"0\", \"values\": [1.9161287833235827, 2.4114632239553027, 3.842059137806608, 4.19088848006689, 2.6521927547112885, 1.8906640235962393]}]}], \"timeStepNo\": 5, \"currentTime\": 0.1}";
+  std::string referenceOutput = "{\"meshType\": \"StructuredRegularFixed\", \"dimension\": 1, \"nElementsGlobal\": [5], \"nElementsLocal\": [5], \"beginNodeGlobalNatural\": [0], \"hasFullNumberOfNodes\": [true], \"basisFunction\": \"Lagrange\", \"basisOrder\": 1, \"onlyNodalValues\": true, \"nRanks\": 1, \"ownRankNo\": 0, \"data\": [{\"name\": \"geometry\", \"components\": [{\"name\": \"x\", \"values\": [0.0, 0.8, 1.6, 2.4000000000000004, 3.2, 4.0]}, {\"name\": \"y\", \"values\": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}, {\"name\": \"z\", \"values\": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}]}, {\"name\": \"solution\", \"components\": [{\"name\": \"0\", \"values\": [2.0429559072490386, 2.2518627527228317, 3.8477024200726957, 4.495048744910582, 2.3533552300645306, 2.0611026396733574]}]}], \"timeStepNo\": 5, \"currentTime\": 0.1}";
   assertFileMatchesContent("out_diffusion1d_implicit_0000004.py", referenceOutput);
+
+}
+
+TEST(DiffusionTest, ImplicitEuler1DPOD)
+{
+  std::string pythonConfig = R"(
+    
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+print("installed packages: ",installed_packages_list)
+
+
+# Diffusion 1D POD
+n = 5   # number of elements
+k = 5   
+
+config = {
+  "ModelOrderReduction": {
+    "nRowsSnapshots" : n,
+    "nReducedBases" : k,   
+    "ImplicitEuler" : {
+       "numberTimeSteps": 5,
+       "endTime": 0.1,
+       "initialValues": [2,2,4,5,2,2],      
+       "FiniteElementMethod" : {
+          "nElements": n,
+          "physicalExtent": 4.0,
+          "relativeTolerance": 1e-15,
+          "diffusionTensor": [5.0],
+       },
+       "OutputWriter" : [
+         #{"format": "Paraview", "outputInterval": 1, "filename": "out", "binaryOutput": "false", "fixedFormat": False},
+         #{"format": "PythonFile", "filename": "out/diffusion1d_implicit", "outputInterval": 1, "binary":false}
+       ]
+    },   
+    "ImplicitEulerReduced" : {
+      "numberTimeSteps": 5,
+      "endTime": 0.1,
+      "initialValues": [2,2,4,5,2,2],      
+      "FiniteElementMethod" : {
+        "nElements": n,
+        "physicalExtent": 4.0,
+        "relativeTolerance": 1e-15,
+        "diffusionTensor": [5.0],
+      },
+      "OutputWriter" : [
+      #{"format": "Paraview", "outputInterval": 1, "filename": "out", "binaryOutput": "false", "fixedFormat": False},
+      {"format": "PythonFile", "filename": "out/diffusion1d_pod", "outputInterval": 100, "binary":True}
+      ]
+    },
+  },
+}
+)";
+
+  DihuContext settings(argc, argv, pythonConfig);
+
+  ModelOrderReduction::ImplicitEulerReduced<
+    TimeSteppingScheme::ImplicitEuler<
+      SpatialDiscretization::FiniteElementMethod<
+        Mesh::StructuredRegularFixedOfDimension<1>,
+        BasisFunction::LagrangeOfOrder<>,
+        Quadrature::None,
+        Equation::Dynamic::IsotropicDiffusion
+      >
+    >
+  > problem(settings);
+
+  problem.run();
+
+  std::string referenceOutput = "{\"meshType\": \"StructuredRegularFixed\", \"dimension\": 1, \"nElementsGlobal\": [5], \"nElementsLocal\": [5], \"beginNodeGlobalNatural\": [0], \"hasFullNumberOfNodes\": [true], \"basisFunction\": \"Lagrange\", \"basisOrder\": 1, \"onlyNodalValues\": true, \"nRanks\": 1, \"ownRankNo\": 0, \"data\": [{\"name\": \"geometry\", \"components\": [{\"name\": \"x\", \"values\": [0.0, 0.2, 0.4, 0.6000000000000001, 0.8, 1.0]}, {\"name\": \"y\", \"values\": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}, {\"name\": \"z\", \"values\": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}]}, {\"name\": \"solution\", \"components\": [{\"name\": \"0\", \"values\": [2.0429559072490378, 2.251862752722829, 3.847702420072692, 4.49504874491058, 2.3533552300645284, 2.0611026396733565]}]}], \"timeStepNo\": 5, \"currentTime\": 0.1}";
+  assertFileMatchesContent("out_diffusion1d_implicit_pod_0000004.py", referenceOutput);
 
 }
 

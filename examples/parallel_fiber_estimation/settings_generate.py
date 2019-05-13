7d2c7c03bf7e0da33ebc7e05ffa2ac8e48bce9bc
Benjamin Maier <maierbn@gmail.com>
2019-05-13 15:07:34 +0200

    Update level in settings

--- examples/parallel_fiber_estimation/settings_generate.py
+++ examples/parallel_fiber_estimation/settings_generate.py
@@ -35,7 +35,7 @@ config = {
     "nElementsZPerSubdomain": 50,         # number of elements in z-direction per subdomain
     "nFineGridFibers": 3,                 # number of additional fine fibers that are interpolated between the main "key" fibers, the key fibers are traced
     "useGradientField": False,            # set to False
-    "maxLevel": 1,                        # maximum level (0=1 process, 1=8 processes, 2=64 processes)
+    "maxLevel": 2,                        # maximum level (0=1 process, 1=8 processes, 2=64 processes)
     "lineStepWidth":  0.1,                # line width for tracing of fibers
     "nNodesPerFiber": (220.-72.) / 0.1,   # number of nodes in each final fiber
     "improveMesh": True,                  # smooth the 2D meshes, required for bigger meshes or larger amount of ranks

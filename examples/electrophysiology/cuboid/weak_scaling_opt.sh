810300bcecd8c2fd113fc680c10d8ffe0da64a78
Tobias <st143615@stud.uni-stuttgart.de>
2019-06-08 13:02:59 +0200

    new values for weak scaling

--- examples/electrophysiology/cuboid/weak_scaling_opt.sh
+++ examples/electrophysiology/cuboid/weak_scaling_opt.sh
@@ -3,67 +3,67 @@
 #../run_tests.py  # this generates the following calls
 #<n_processes_per_fiber> <n_fibers> <n_nodes_per_fiber> <scenario_name> <solver_type> <preconditioner_type>
 
-mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 100 solver_scaling gmres none
-mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 400 solver_scaling gmres none
-mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 800 solver_scaling gmres none
-mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 1800 solver_scaling gmres none
-mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 3000 solver_scaling gmres none   
-mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 3600 solver_scaling gmres none     
-mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 4200 solver_scaling gmres none  
-mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 4800 solver_scaling gmres none   
-mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 5400 solver_scaling gmres none    
-mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 6000 solver_scaling gmres none 
-mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 6600 solver_scaling gmres none    
-mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 7200 solver_scaling gmres none 
+mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 250 solver_scaling gmres none
+mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 1000 solver_scaling gmres none
+mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 2000 solver_scaling gmres none
+mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 4500 solver_scaling gmres none
+mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 7500 solver_scaling gmres none   
+mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 9000 solver_scaling gmres none     
+mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 10500 solver_scaling gmres none  
+mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 12000 solver_scaling gmres none   
+mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 13500 solver_scaling gmres none    
+mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 15000 solver_scaling gmres none 
+mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 16500 solver_scaling gmres none    
+mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 18000 solver_scaling gmres none 
 
-mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 100 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 400 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 800 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 1800 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 3000 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 3600 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 4200 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 4800 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 5400 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 6000 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 6600 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 7200 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 250 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 1000 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 2000 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 4500 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 7500 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 9000 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 10500 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 12000 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 13500 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 15000 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 16500 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 18000 solver_scaling cg pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type HMIS -pc_hypre_boomeramg_agg_nl 2 -pc_hypre_boomeramg_strong_threshold 0.3
 
-mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 100 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 400 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 800 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 1800 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg-pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 3000 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 3600 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 4200 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 4800 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 5400 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 6000 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 6600 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
-mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 7200 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 250 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 1000 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 2000 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 4500 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg-pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 7500 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 9000 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 10500 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 12000 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 13500 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 15000 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 16500 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
+mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 18000 solver_scaling richardson pchypre -pc_type hypre -pc_hypre_type boomeramg -pc_hypre_boomeramg_coarsen_type Falgout -pc_hypre_boomeramg_agg_nl 0 -pc_hypre_boomeramg_strong_threshold 0.3
 
-mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 100 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 400 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 800 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 1800 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 3000 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 3600 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 4200 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 4800 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 5400 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 6000 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 6600 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 7200 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 250 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 1000 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 2000 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 4500 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 7500 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 9000 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 10500 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 12000 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 13500 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 15000 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 16500 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 18000 solver_scaling cg gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
 
-mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 100 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 400 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 800 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 1800 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 3000 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 3600 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 4200 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 4800 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 5400 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 6000 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 6600 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
-mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 7200 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 1 ./cuboid ../cuboid_settings.py 1 1 250 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 4 ./cuboid ../cuboid_settings.py 4 1 1000 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 8 ./cuboid ../cuboid_settings.py 8 1 2000 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 18 ./cuboid ../cuboid_settings.py 18 1 4500 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 30 ./cuboid ../cuboid_settings.py 30 1 7500 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 36 ./cuboid ../cuboid_settings.py 36 1 9000 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 42 ./cuboid ../cuboid_settings.py 42 1 10500 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 48 ./cuboid ../cuboid_settings.py 48 1 12000 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 54 ./cuboid ../cuboid_settings.py 54 1 13500 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 60 ./cuboid ../cuboid_settings.py 60 1 15000 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 66 ./cuboid ../cuboid_settings.py 66 1 16500 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06
+mpiexec -n 72 ./cuboid ../cuboid_settings.py 72 1 18000 solver_scaling preonly gamg -pc_gamg_square_graph 4 -pc_gamg_threshold 0.06

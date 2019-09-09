598060d4007793f4853f0c215c5db9e8cd1127d2
Benjamin Maier <maier.bn@gmail.com>
2019-09-09 15:30:14 +0200

    Add missing template argument for hyperelastictiy

--- examples/solid_mechanics/hyperelasticity/src/3d_hyperelasticity.cpp
+++ examples/solid_mechanics/hyperelasticity/src/3d_hyperelasticity.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
   DihuContext settings(argc, argv);
   
   // define problem
-  SpatialDiscretization::HyperelasticitySolver problem(settings);
+  SpatialDiscretization::HyperelasticitySolver<> problem(settings);
   
   // run problem
   problem.run();

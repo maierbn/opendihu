91483dc38898655e85a1fa1581c20f2eceac11b8
Benjamin Maier <maierbn@gmail.com>
2019-10-22 17:18:05 +0200

    Update settings file

--- examples/electrophysiology/fibers_emg/variables/review_bw_stiftung.py
+++ examples/electrophysiology/fibers_emg/variables/review_bw_stiftung.py
@@ -46,38 +46,38 @@ Cm = 0.58                           # [uF/cm^2] membrane capacitance, (1 = fast 
 
 import random
 motor_units = [
-  {"radius": 40.00, "activation_start_time": 0.0, "stimulation_frequency": 23.92, "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},    # low number of fibers
-  {"radius": 42.35, "activation_start_time": 0.2, "stimulation_frequency": 23.36, "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},
-  {"radius": 45.00, "activation_start_time": 0.4, "stimulation_frequency": 23.32, "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},
-  {"radius": 48.00, "activation_start_time": 0.6, "stimulation_frequency": 22.46, "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},
-  {"radius": 51.42, "activation_start_time": 0.8, "stimulation_frequency": 20.28, "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},
-  {"radius": 55.38, "activation_start_time": 1.0, "stimulation_frequency": 16.32, "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},
-  {"radius": 60.00, "activation_start_time": 1.2, "stimulation_frequency": 12.05, "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},
-  {"radius": 65.45, "activation_start_time": 1.4, "stimulation_frequency": 10.03, "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},
-  {"radius": 72.00, "activation_start_time": 1.6, "stimulation_frequency": 8.32,  "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},
-  {"radius": 80.00, "activation_start_time": 1.8, "stimulation_frequency": 7.66,  "jitter": [0.1*random.randrange(-1,1) for i in range(100)]},    # high number of fibers
+  {"radius": 40.00, "activation_start_time": 0.0, "stimulation_frequency": 23.92, "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},    # low number of fibers
+  {"radius": 42.35, "activation_start_time": 0.2, "stimulation_frequency": 23.36, "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},
+  {"radius": 45.00, "activation_start_time": 0.4, "stimulation_frequency": 23.32, "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},
+  {"radius": 48.00, "activation_start_time": 0.6, "stimulation_frequency": 22.46, "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},
+  {"radius": 51.42, "activation_start_time": 0.8, "stimulation_frequency": 20.28, "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},
+  {"radius": 55.38, "activation_start_time": 1.0, "stimulation_frequency": 16.32, "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},
+  {"radius": 60.00, "activation_start_time": 1.2, "stimulation_frequency": 12.05, "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},
+  {"radius": 65.45, "activation_start_time": 1.4, "stimulation_frequency": 10.03, "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},
+  {"radius": 72.00, "activation_start_time": 1.6, "stimulation_frequency": 8.32,  "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},
+  {"radius": 80.00, "activation_start_time": 1.8, "stimulation_frequency": 7.66,  "jitter": [0.1*random.uniform(-1,1) for i in range(100)]},    # high number of fibers
 ]
 
 # timing parameters
 # -----------------
-end_time = 4.0                      # [ms] end time of the simulation
+end_time = 4000.0                      # [ms] end time of the simulation
 stimulation_frequency = 100*1e-3    # [ms^-1] sampling frequency of stimuli in firing_times_file, in stimulations per ms, number before 1e-3 factor is in Hertz.
 stimulation_frequency_jitter = 0    # [-] jitter in percent of the frequency, added and substracted to the stimulation_frequency after each stimulation
-dt_0D = 2e-4                        # [ms] timestep width of ODEs
-dt_1D = 2e-3                        # [ms] timestep width of diffusion
-dt_splitting = 2e-3                 # [ms] overall timestep width of strang splitting
+dt_0D = 2e-3                        # [ms] timestep width of ODEs
+dt_1D = 4e-3                        # [ms] timestep width of diffusion
+dt_splitting = 4e-3                 # [ms] overall timestep width of strang splitting
 dt_3D = 0.5                         # [ms] time step width of coupling, when 3D should be performed, also sampling time of monopolar EMG
 output_timestep = 0.5               # [ms] timestep for output files
 
 # other options
 paraview_output = True
-fiber_file="../../input/2x2fibers.bin"
+fiber_file="../../input/37x37fibers.bin"
 firing_times_file="../../input/MU_firing_times_always.txt"    # use setSpecificStatesCallEnableBegin and setSpecificStatesCallFrequency
 fiber_distribution_file="../../input/MU_fibre_distribution_10MUs.txt"
 
 # functions, here, Am, Cm and Conductivity are constant for all fibers and MU's
 def get_am(fiber_no, mu_no):
-  r = motor_units[mu_no]["radius"]
+  r = motor_units[mu_no]["radius"]*1e-2
   # cylinder surface: A = 2*π*r*l, V = cylinder volume: π*r^2*l, Am = A/V = 2*π*r*l / (π*r^2*l) = 2/r
   return 2./r
 
@@ -88,11 +88,11 @@ def get_conductivity(fiber_no, mu_no):
   return Conductivity
 
 def get_specific_states_call_frequency(fiber_no, mu_no):
-  stimulation_frequency = motor_units[mu_no]["stimulation_frequency"]
-  return stimulation_frequency
+  stimulation_frequency = motor_units[mu_no % len(motor_units)]["stimulation_frequency"]
+  return stimulation_frequency*1e-3
 
 def get_specific_states_frequency_jitter(fiber_no, mu_no):
   return motor_units[mu_no % len(motor_units)]["jitter"]
 
 def get_specific_states_call_enable_begin(fiber_no, mu_no):
-  return motor_units[mu_no]["activation_start_time"]
+  return motor_units[mu_no % len(motor_units)]["activation_start_time"]*1e3

4f84205b890212808f2dd4a16e0383832ffba659
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2019-10-25 16:13:00 +0200

    set bidomain solver tolerance to 1e-5

--- examples/electrophysiology/fibers_emg/settings_fibers_emg.py
+++ examples/electrophysiology/fibers_emg/settings_fibers_emg.py
@@ -145,7 +145,7 @@ config = {
       "dumpFormat":         "matlab",
     },
     "activationSolver": {   # solver for the static Bidomain equation and the EMG
-      "relativeTolerance":  1e-12,
+      "relativeTolerance":  1e-5,
       "maxIterations":      1e4,
       "solverType":         variables.emg_solver_type,
       "preconditionerType": variables.emg_preconditioner_type,

1c51ab9e4b30992d926eaf8e7601642486095a4e
NietzscheM <marius.nitzsche@gmail.com>
2020-02-05 15:15:02 +0100

    neuster Stand

--- examples/diffusion/PinT_diffusion1d/settings_PinT.py
+++ examples/diffusion/PinT_diffusion1d/settings_PinT.py
@@ -68,6 +68,7 @@ config = {
      "dumpFormat": "matlab",         # the file format of the dump file, "matlab", "ascii" or "default"
    },
 },
+# for k in range (n):
 "Meshes":{
     "mesh_{}".format(k): {
       "nElements": (2 ** k)-1,                 # number of elements
@@ -81,30 +82,33 @@ config = {
     "myOption": 42,                   # example option that is parsed in the constructor
     "option1": "blabla",              # another example option that is parsed in the data object
 
-     "ImplicitEuler" : {
-        "numberTimeSteps": 5,
-        "endTime": 0.1,
-        "initialValues": [2,2,4,5,2,2],    # the initial values
-        "dirichletBoundaryConditions": {}, # Dirichlet boundary conditions as dict
-        "inputMeshIsGlobal": True,         # initial values and BC's are given for all dofs, even if executed in parallel
-        "timeStepOutputInterval": 1,       # how often to print the current timestep to console
-        "nAdditionalFieldVariables": 0,    # for more complex nested solvers, the number of additional field variables that will be transferred without being touched
-        "solverName": "linearSolver",      # the solver to use, referes to what was defined under "Solvers"
+    "TimeSteppingScheme": [
+      {"ImplicitEuler": {
+            "numberTimeSteps": 5,
+            "endTime": 0.1,
+            "initialValues": [2,2,4,5,2,2],    # the initial values
+            "dirichletBoundaryConditions": {}, # Dirichlet boundary conditions as dict
+            "inputMeshIsGlobal": True,         # initial values and BC's are given for all dofs, even if executed in parallel
+            "timeStepOutputInterval": 1,       # how often to print the current timestep to console
+            "nAdditionalFieldVariables": 0,    # for more complex nested solvers, the number of additional field variables that will be transferred without being touched
+            "solverName": "linearSolver",      # the solver to use, referes to what was defined under "Solvers"
 
-        "FiniteElementMethod" : {
-           # mesh
-           "meshName": "mesh_{}".format(4),
-           #"nElements": n,                 # number of elements
-           #"physicalExtent": 4.0,          # the physical size of the domain
-           "solverName": "linearSolver",   # the solver to use, referes to what was defined under "Solvers"
-           "prefactor": 5.0,               # the prefactor 'c' of 'du/dt = c du^2/dx^2'
-           "inputMeshIsGlobal": True,      # boundary conditions are given as global indices
-           "nAdditionalFieldVariables": 0, # for more complex nested solvers, the number of additional field variables that will be transferred without being touched
-        },
-        "OutputWriter" : [
-          #{"format": "Paraview", "outputInterval": 1, "filename": "out", "binaryOutput": "false", "fixedFormat": False, "onlyNodalValues": True},
-          {"format": "PythonFile", "filename": "out/diffusion1d_implicit", "outputInterval": 1, "binary":False, "onlyNodalValues": True}
-        ]
-     }
+            "FiniteElementMethod" : {
+               # mesh
+               "meshName": "mesh_{}".format(k),
+               #"nElements": n,                 # number of elements
+               #"physicalExtent": 4.0,          # the physical size of the domain
+               "solverName": "linearSolver",   # the solver to use, referes to what was defined under "Solvers"
+               "prefactor": 5.0,               # the prefactor 'c' of 'du/dt = c du^2/dx^2'
+               "inputMeshIsGlobal": True,      # boundary conditions are given as global indices
+               "nAdditionalFieldVariables": 0, # for more complex nested solvers, the number of additional field variables that will be transferred without being touched
+            },
+            "OutputWriter" : [
+              #{"format": "Paraview", "outputInterval": 1, "filename": "out", "binaryOutput": "false", "fixedFormat": False, "onlyNodalValues": True},
+              {"format": "PythonFile", "filename": "out/diffusion1d_implicit", "outputInterval": 1, "binary":False, "onlyNodalValues": True}
+            ]
+          }
+        } for k in range(n)],
+ # for j in range(n)
  }
 }
--- examples/diffusion/PinT_diffusion1d/src/PinT_IE.cpp
+++ examples/diffusion/PinT_diffusion1d/src/PinT_IE.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
         BasisFunction::LagrangeOfOrder<>,
         Quadrature::None,
         Equation::Dynamic::IsotropicDiffusion
-        >
+      >
     >
   > problem(settings);
 

c9846c4f20beeb1f70533626ba02c8fd46482581
Benjamin Maier <maierbn@gmail.com>
2020-05-04 15:52:56 +0200

    Update package for lapack

--- user-variables.scons.py
+++ user-variables.scons.py
@@ -144,7 +144,7 @@ if False:
   MPI_DOWNLOAD = True
   MPI_IGNORE_MPICC = True    # this downloads and builds openmpi
 
-PETSC_DEBUG = True            # this enables debugging flags such that valgrind memcheck can track MPI errors
+#PETSC_DEBUG = True            # this enables debugging flags such that valgrind memcheck can track MPI errors
 
 # specialized settings for supercomputer (HazelHen)
 import os

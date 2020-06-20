453f0b7d0383b365b96eedd5c0b01edae80ecf29
Benjamin Maier <maierbn@gmail.com>
2020-06-20 13:44:53 +0200

    Update helper shortcut scripts

--- scripts/shortcuts/mkorn.sh
+++ scripts/shortcuts/mkorn.sh
@@ -1,1 +1,1 @@
-cd $OPENDIHU_HOME && time scons BUILD_TYPE=release no_tests=True -j 4 $* && (cd - && echo opendihu release build without tests succeeded) || (echo opendihu release build without tests failed && cd - && exit -1)
+cd $OPENDIHU_HOME && time python2.7 $OPENDIHU_HOME/dependencies/scons/scons.py BUILD_TYPE=release no_tests=True -j 4 $* && (cd - && echo opendihu release build without tests succeeded) || (echo opendihu release build without tests failed && cd - && exit -1)

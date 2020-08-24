db56cc4f5a221b4bade3571fa1aef80f2446acb1
Benjamin Maier <maier.bn@gmail.com>
2020-08-24 17:21:40 +0200

    Fix plotting script for emg.

--- examples/electrophysiology/fibers/fibers_fat_emg/plot_emg.py
+++ examples/electrophysiology/fibers/fibers_fat_emg/plot_emg.py
@@ -26,27 +26,76 @@ else:
   filenames = [os.path.join(path,f) for f in filenames if ".csv" in f and "found" not in f]
 filename = filenames[0]
 
-print("file: {}".format(filename))
-
 # parse csv file
 data = np.genfromtxt(filename, delimiter=';')
 t_list = data[:,1]
   
 n_points = (int)(data[0][2])
-print("n_points:",n_points)
 
 # determine number of electrodes in x direction
-z_positions = np.array([data[0][3+3*i+2] for i in range(n_points)])
-differences = z_positions[1:] - z_positions[0:-1]
-jumps = np.where(differences > np.mean(differences)*3)[0]
-n_points_z = jumps[0] + 1
-n_points_xy = (int)(n_points / n_points_z)
+# extract all z positions (along muscle)
+position_data = data[0][3:3+3*n_points]
+electrode_positions = [np.array(position_data[3*i:3*i+3]) for i in range(n_points)]
+z_positions = np.array([position_data[3*i+2] for i in range(n_points)])
 
+# compute differences in z position between neighbour points, this will have a jump (high increment) where a new line starts
+differences = z_positions[1:] - z_positions[0:-1]
+
+# determine the indices where the increment is higher than 3 times the mean 
+jumps = np.where(differences > np.mean(differences)*3)[0]
+
+# number of points in x/y-direction (across muscle) is position of first jump + 1
+n_points_xy = jumps[0] + 1
+n_points_z = (int)(n_points / n_points_xy)
+
+# plot electrode positions for debugging
+if False:
+  from mpl_toolkits.mplot3d import Axes3D
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+
+  xpos = [electrode_positions[i][0] for i in range(n_points)]
+  ypos = [electrode_positions[i][1] for i in range(n_points)]
+  zpos = [electrode_positions[i][2] for i in range(n_points)]
+  colors = [(i/n_points,i/n_points,i/n_points) for i in range(n_points)]
+  ax.scatter(xpos,ypos,zpos,c=colors)
+  plt.show()
+
+# compute average interelectrode distances
+# across muscle (xy)
+distances_xy = []
+for j in range(n_points_z):
+  for i in range(n_points_xy-1):
+    point_index = j*n_points_xy + i
+    
+    p0 = electrode_positions[point_index]
+    p1 = electrode_positions[point_index+1]
+    
+    distance = np.linalg.norm(p0-p1)
+    distances_xy.append(distance)
+
+# along muscle (z)
+distances_z = []
+for j in range(n_points_z-1):
+  for i in range(n_points_xy):
+    point_index = j*n_points_xy + i
+    
+    p0 = electrode_positions[point_index]
+    p1 = electrode_positions[point_index+n_points_xy]
+    
+    distance = np.linalg.norm(p0-p1)
+    distances_z.append(distance)
+
+# compute inter eletrcode distances in cm
+inter_electrode_distance_xy = np.mean(distances_xy)
+inter_electrode_distance_z = np.mean(distances_z)
+
+print("Input file \"{}\" contains {} x {} = {} points".format(filename, n_points_xy, n_points_z, n_points))
+print("Inter-electrode distance: {:.2f} mm x {:.2f} mm".format(10*inter_electrode_distance_xy, 10*inter_electrode_distance_z))
+
+# create plot
 n_plots_x = n_points_xy
 n_plots_y = n_points_z
-
-print("{} x {} points detected".format(n_points_xy, n_points_z))
-
 fig = plt.figure(figsize=(5,10))
 
 # labels for entire plot
@@ -71,7 +120,7 @@ for j in range(n_plots_y):
     
     emg_list = data[:,3+n_points*3+index]
     
-    ax = fig.add_subplot(n_plots_x,n_plots_y,index+1)   # nrows, ncolrs, index, plots start top left and increase to the right
+    ax = fig.add_subplot(n_plots_y,n_plots_x,index+1)   # nrows, ncols, index, plots start top left and increase to the right
     ax.plot(t_list, emg_list)
     ax.set_ylim(minimum_value,maximum_value)
     #ax.axis('off')

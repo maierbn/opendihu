932052988fe60b4508d5f157fe729c91dd568937
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2020-09-18 09:14:36 +0200

    Revert changes needed for score-p

--- scripts/fake-scorep.sh
+++ scripts/fake-scorep.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#g++ $@
+
+if true; then
 if [[ $@ == *"03_generator_vc"* ]]; then
   echo using g++ on $@
   g++ $@
 else
   echo using scorep on $@
-  scorep --user --instrument-filter=scripts/compile-time-filter.filt g++ $@
+  scorep-g++ $@
+fi
 fi

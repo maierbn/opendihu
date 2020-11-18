4da1e335630a98d5cb6b0c16ad151a845bd56fc9
Benjamin Maier <maierbn@gmail.com>
2020-11-18 14:38:02 +0100

    Update scons-config script for precice

--- dependencies/scons-config/sconsconfig/packages/precice.py
+++ dependencies/scons-config/sconsconfig/packages/precice.py
@@ -74,7 +74,7 @@ class precice(Package):
     self.number_output_lines = 15536
       
     self.libs = ['precice']
-    self.extra_libs = [[], ['boost_filesystem' 'boost_log' 'boost_log_setup' 'boost_program_options' 'boost_system' 'boost_thread' 'boost_unit_test_framework'],
+    self.extra_libs = [[], ['boost_filesystem', 'boost_log', 'boost_log_setup', 'boost_program_options', 'boost_system', 'boost_thread', 'boost_unit_test_framework'],
     									 ['boost_atomic', 'boost_chrono', 'boost_date_time', 'boost_filesystem', 'boost_log', 'boost_log_setup', 'boost_prg_exec_monitor', 'boost_program_options', 'boost_regex', 'boost_system', 'boost_test_exec_monitor', 'boost_thread', 'boost_timer', 'boost_unit_test_framework']]
     self.headers = ["precice/SolverInterface.hpp"]
 
@@ -83,13 +83,14 @@ class precice(Package):
     ctx.Message('Checking for preCICE ...       ')
     self.check_options(env)
 
-    # only build boost if it was not yet build
+    # first try is using a system boost
     self.set_build_handler([
       'mkdir -p ${PREFIX}/include',
+
+      # Eigen
       'cd ${SOURCE_DIR} && wget https://gitlab.com/libeigen/eigen/-/archive/3.3.8/eigen-3.3.8.tar.gz && tar xf eigen-3.3.8.tar.gz && ln -s ${SOURCE_DIR}/eigen-3.3.8/Eigen ${PREFIX}/include/Eigen',   # eigen
-      '[ ! -d ${PREFIX}/include/boost ] && (cd ${SOURCE_DIR} && [ ! -f ${SOURCE_DIR}/boost_1_65_1.tar.gz ] && ( wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz && tar xf boost_1_65_1.tar.gz ); \
-      cd boost_1_65_1 && ./bootstrap.sh --with-libraries=log,thread,system,filesystem,program_options,test --prefix=${PREFIX} && \
-      ./b2 install )',
+
+      # precice
       'cd ${SOURCE_DIR} && mkdir -p build && cd build && '+ctx.env["cmake"]+' -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_BUILD_TYPE=RELEASE -DPYTHON_EXECUTABLE=${DEPENDENCIES_DIR}/python/install/bin/python3 \
       -DPETSC_DIR=${PETSC_DIR} -DPETSC_EXECUTABLE_RUNS=TRUE \
@@ -98,6 +99,36 @@ class precice(Package):
       cd ${SOURCE_DIR}/build && make precice install -j 4'
     ])
     
+    res = super(precice, self).check(ctx)
+  
+    # if installation of petsc failed with the current command, retry with different options 
+    if not res[0]:
+      ctx.Log('Retry (1) with manually building boost\n')
+      ctx.Message('Retry (1) with manually building boost.')
+
+      # only build boost if it was not yet build
+      self.set_build_handler([
+        'mkdir -p ${PREFIX}/include',
+
+        # Eigen
+        'cd ${SOURCE_DIR} && wget https://gitlab.com/libeigen/eigen/-/archive/3.3.8/eigen-3.3.8.tar.gz && tar xf eigen-3.3.8.tar.gz && ln -s ${SOURCE_DIR}/eigen-3.3.8/Eigen ${PREFIX}/include/Eigen',
+
+        # boost
+        '[ ! -d ${PREFIX}/include/boost ] && (cd ${SOURCE_DIR} && [ ! -f ${SOURCE_DIR}/boost_1_65_1.tar.gz ] && \
+          ( wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz && tar xf boost_1_65_1.tar.gz ); \
+          cd boost_1_65_1 && ./bootstrap.sh --with-libraries=log,thread,system,filesystem,program_options,test --prefix=${PREFIX} && \
+          ./b2 install )',
+
+        # precice
+        'cd ${SOURCE_DIR} && mkdir -p build && cd build && '+ctx.env["cmake"]+' -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_BUILD_TYPE=RELEASE -DPYTHON_EXECUTABLE=${DEPENDENCIES_DIR}/python/install/bin/python3 \
+        -DPETSC_DIR=${PETSC_DIR} -DPETSC_EXECUTABLE_RUNS=TRUE \
+        -DLIBXML2_INCLUDE_DIR=${LIBXML2_DIR}/include -DLIBXML2_LIBRARY=${LIBXML2_DIR}/lib/libxml2.so -DPRECICE_ENABLE_FORTRAN=OFF \
+        -DMPI_CXX_COMPILER='+ctx.env["mpiCC"]+' -DPETSC_COMPILER='+ctx.env["mpiCC"]+' -DMPI_DIR=$MPI_DIR .. && \
+        cd ${SOURCE_DIR}/build && make precice install -j 4'
+      ])  
+  
+    self.check_options(env)
     res = super(precice, self).check(ctx)
 
     self.check_required(res[0], ctx)

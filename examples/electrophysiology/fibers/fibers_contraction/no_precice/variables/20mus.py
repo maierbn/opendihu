cdeff6a8ecbf59292cadf2ecfe1cf54460f9db6e
Benjamin Maier <maierbn@gmail.com>
2020-11-18 14:20:08 +0100

    Add scenario file for 20mus

---
+++ examples/electrophysiology/fibers/fibers_contraction/no_precice/variables/20mus.py
@@ -0,0 +1,202 @@
+
+# scenario name for log file
+scenario_name = "20mus"
+
+# Fixed units in cellMl models:
+# These define the unit system.
+# 1 cm = 1e-2 m
+# 1 ms = 1e-3 s
+# 1 uA = 1e-6 A
+# 1 uF = 1e-6 F
+# 
+# derived units:
+#   (F=s^4*A^2*m^-2*kg^-1) => 1 ms^4*uA^2*cm^-2*x*kg^-1 = (1e-3)^4 s^4 * (1e-6)^2 A^2 * (1e-2)^-2 m^-2 * (x)^-1 kg^-1 = 1e-12 * 1e-12 * 1e4 F = 1e-20 * x^-1 F := 1e-6 F => x = 1e-14
+# 1e-14 kg = 10e-15 kg = 10e-12 g = 10 pg
+
+# (N=kg*m*s^-2) => 1 10pg*cm*ms^2 = 1e-14 kg * 1e-2 m * (1e-3)^-2 s^-2 = 1e-14 * 1e-2 * 1e6 N = 1e-10 N = 10 nN
+# (S=kg^-1*m^-2*s^3*A^2, Siemens not Sievert!) => (1e-14*kg)^-1*cm^-2*ms^3*uA^2 = (1e-14)^-1 kg^-1 * (1e-2)^-2 m^-2 * (1e-3)^3 s^3 * (1e-6)^2 A^2 = 1e14 * 1e4 * 1e-9 * 1e-12 S = 1e-3 S = 1 mS
+# (V=kg*m^2*s^-3*A^-1) => 1 10pg*cm^2*ms^-3*uA^-1 = (1e-14) kg * (1e-2)^2 m^2 * (1e-3)^-3 s^-3 * (1e-6)^-1 A^-1 = 1e-14 * 1e-4 * 1e6 * 1e6 V = 1e-6 V = 1mV
+# (Hz=s^-1) => 1 ms^-1 = (1e-3)^-1 s^-1 = 1e3 Hz
+# (kg/m^3) => 1 10 pg/cm^3 = 1e-14 kg / (1e-2 m)^3 = 1e-14 * 1e6 kg/m^3 = 1e-8 kg/m^3
+# (Pa=kg/(m*s^2)) => 1e-14 kg / (1e-2 m * 1e-3^2 s^2) = 1e-14 / (1e-8) Pa = 1e-6 Pa
+
+# Hodgkin-Huxley
+# t: ms
+# STATES[0], Vm: mV
+# CONSTANTS[1], Cm: uF*cm^-2
+# CONSTANTS[2], I_Stim: uA*cm^-2
+# -> all units are consistent
+
+# Shorten
+# t: ms
+# CONSTANTS[0], Cm: uF*cm^-2
+# STATES[0], Vm: mV
+# ALGEBRAIC[32], I_Stim: uA*cm^-2
+# -> all units are consistent
+
+# Fixed units in mechanics system
+# 1 cm = 1e-2 m
+# 1 ms = 1e-3 s
+# 1 N
+# 1 N/cm^2 = (kg*m*s^-2) / (1e-2 m)^2 = 1e4 kg*m^-1*s^-2 = 10 kPa
+# (kg = N*s^2*m^-1) => N*ms^2*cm^-1 = N*(1e-3 s)^2 * (1e-2 m)^-1 = 1e-4 N*s^2*m^-1 = 1e-4 kg
+# (kg/m^3) => 1 * 1e-4 kg * (1e-2 m)^-3 = 1e2 kg/m^3
+# (m/s^2) => 1 cm/ms^2 = 1e-2 m * (1e-3 s)^-2 = 1e4 m*s^-2
+
+# material parameters
+# --------------------
+# quantities in mechanics unit system
+rho = 10                    # [1e-4 kg/cm^3] density of the muscle (density of water)
+
+# Mooney-Rivlin parameters [c1,c2,b,d] of c1*(Ibar1 - 3) + c2*(Ibar2 - 3) + b/d (λ - 1) - b*ln(λ)
+# Heidlauf13: [6.352e-10 kPa, 3.627 kPa, 2.756e-5 kPa, 43.373] = [6.352e-11 N/cm^2, 3.627e-1 N/cm^2, 2.756e-6 N/cm^2, 43.373], pmax = 73 kPa = 7.3 N/cm^2
+# Heidlauf16: [3.176e-10 N/cm^2, 1.813 N/cm^2, 1.075e-2 N/cm^2, 9.1733], pmax = 7.3 N/cm^2
+
+c1 = 3.176e-10              # [N/cm^2]
+c2 = 1.813                  # [N/cm^2]
+b  = 1.075e-2               # [N/cm^2] anisotropy parameter
+d  = 9.1733                 # [-] anisotropy parameter
+
+# for debugging, b = 0 leads to normal Mooney-Rivlin
+b = 0
+
+material_parameters = [c1, c2, b, d]   # material parameters
+pmax = 7.3                  # [N/cm^2] maximum isometric active stress
+
+# load
+constant_body_force = (0,0,-9.81e-4)   # [cm/ms^2], gravity constant for the body force
+bottom_traction = [0.0,0.0,0.0]        # [N]
+
+# Monodomain parameters
+# --------------------
+# quantities in CellML unit system
+sigma_f = 8.93              # [mS/cm] conductivity in fiber direction (f)
+sigma_xf = 0                # [mS/cm] conductivity in cross-fiber direction (xf)
+sigma_e_f = 6.7             # [mS/cm] conductivity in extracellular space, fiber direction (f)
+sigma_e_xf = 3.35           # [mS/cm] conductivity in extracellular space, cross-fiber direction (xf) / transverse
+
+Conductivity = 3.828      # [mS/cm] sigma, conductivity
+Am = 500.0                  # [cm^-1] surface area to volume ratio
+Cm = 0.58                   # [uF/cm^2] membrane capacitance, (1 = fast twitch, 0.58 = slow twitch)
+# diffusion prefactor = Conductivity/(Am*Cm)
+
+# timing and activation parameters
+# -----------------
+# motor units from paper Klotz2019 "Modelling the electrical activity of skeletal muscle tissue using a multi‐domain approach"
+import numpy as np
+import random
+random.seed(0)  # ensure that random numbers are the same on every rank
+
+n_motor_units = 20   # number of motor units
+
+motor_units = []
+for mu_no in range(n_motor_units):
+
+  # capacitance of the membrane
+  if mu_no <= 7:
+    cm = 0.58    # slow twitch (type I)
+  else:
+    cm = 1.0     # fast twitch (type II)
+
+  # fiber radius between 40 and 55 [μm]
+  min_value = 40
+  max_value = 55
+  
+  # ansatz value(i) = c1 + c2*exp(i),
+  # value(0) = min = c1 + c2  =>  c1 = min - c2
+  # value(n-1) = max = min - c2 + c2*exp(n-1)  =>  max = min + c2*(exp(n-1) - 1)  =>  c2 = (max - min) / (exp(n-1) - 1)
+  c2 = (max_value - min_value) / (np.exp(n_motor_units-1) - 1)
+  c1 = min_value - c2
+  radius = c1 + c2*np.exp(mu_no)
+
+  # stimulation frequency [Hz] between 24 and 7
+  min_value = 7 
+  max_value = 24
+  
+  c2 = (max_value - min_value) / (np.exp(n_motor_units-1) - 1)
+  c1 = min_value - c2
+  stimulation_frequency = c1 + c2*np.exp(n_motor_units-1-mu_no)
+
+  # exponential distribution: low number of fibers per MU, slow twitch (type I), activated first --> high number of fibers per MU, fast twitch (type II), activated last
+  motor_units.append(
+  {
+    "radius":                radius,                 # [μm] parameter for motor unit: radius of the fiber, used to compute Am
+    "cm":                    cm,                     # [uF/cm^2] parameter Cm
+    "activation_start_time": 0.1*mu_no,              # [s] when to start activating this motor unit, here it is a ramp
+    "stimulation_frequency": stimulation_frequency,  # [Hz] stimulation frequency for activation
+    "jitter": [0.1*random.uniform(-1,1) for i in range(100)]     # [-] random jitter values that will be added to the intervals to simulate jitter
+  })  
+
+# timing parameters
+# -----------------
+end_time = 4000.0                      # [ms] end time of the simulation
+stimulation_frequency = 100*1e-3    # [ms^-1] sampling frequency of stimuli in firing_times_file, in stimulations per ms, number before 1e-3 factor is in Hertz.
+stimulation_frequency_jitter = 0    # [-] jitter in percent of the frequency, added and substracted to the stimulation_frequency after each stimulation
+dt_0D = 1e-4                        # [ms] timestep width of ODEs (1e-3)
+dt_1D = 1e-4                        # [ms] timestep width of diffusion (1e-3)
+dt_splitting = 1e-4                 # [ms] overall timestep width of strang splitting (1e-3)
+dt_3D = 1                           # [ms] time step width of coupling, when 3D should be performed, also sampling time of monopolar EMG
+output_timestep_fibers = 4e0       # [ms] timestep for fiber output, 0.5
+output_timestep_3D = 4e0              # [ms] timestep for output of fibers and mechanics, should be a multiple of dt_3D
+
+
+# input files
+#fiber_file = "../../../../input/left_biceps_brachii_9x9fibers.bin"
+fiber_file = "../../../../input/left_biceps_brachii_37x37fibers.bin"
+fat_mesh_file = fiber_file + "_fat.bin"
+firing_times_file = "../../../../input/MU_firing_times_always.txt"    # use setSpecificStatesCallEnableBegin and setSpecificStatesCallFrequency
+fiber_distribution_file = "../../../../input/MU_fibre_distribution_37x37_20.txt"
+cellml_file             = "../../../../input/new_slow_TK_2014_12_08.c"
+
+# stride for sampling the 3D elements from the fiber data
+# a higher number leads to less 3D elements
+sampling_stride_x = 3
+sampling_stride_y = 3
+sampling_stride_z = 40      # good values: divisors of 1480: 1480 = 1*1480 = 2*740 = 4*370 = 5*296 = 8*185 = 10*148 = 20*74 = 37*40
+
+# HD-EMG electrode parameters
+fiber_file_for_hdemg_surface = fat_mesh_file    # use the fat mesh for placing electrodes, this option is the file of the 2D mesh on which electrode positions are set
+hdemg_electrode_faces = ["1+"]                  # which faces of this 2D mesh should be considered for placing the HD-EMG electrodes (list of faces, a face is one of "0-" (left), "0+" (right), "1-" (front), "1+" (back))
+
+# xy-direction = across muscle, z-direction = along muscle
+hdemg_electrode_offset_xy = 2.0           # [cm] offset from border of 2D mesh where the electrode array begins
+hdemg_inter_electrode_distance_z = 0.4    # [cm] distance between electrodes ("IED") in z direction (direction along muscle)
+hdemg_inter_electrode_distance_xy = 0.4   # [cm] distance between electrodes ("IED") in transverse direction
+hdemg_n_electrodes_z = 32           # number of electrodes in z direction (direction along muscle)
+hdemg_n_electrodes_xy = 12          # number of electrode across muscle
+
+# other options
+paraview_output = True
+adios_output = False
+exfile_output = False
+python_output = False
+disable_firing_output = False
+fast_monodomain_solver_optimizations = True # enable the optimizations in the fast multidomain solver
+use_analytic_jacobian = True        # If the analytic jacobian should be used for the mechanics problem.
+use_vc = True                       # If the vc optimization type should be used for CellmlAdapter
+
+# functions, here, Am, Cm and Conductivity are constant for all fibers and MU's
+def get_am(fiber_no, mu_no):
+  # get radius in cm, 1 μm = 1e-6 m = 1e-4*1e-2 m = 1e-4 cm
+  r = motor_units[mu_no % len(motor_units)]["radius"]*1e-4
+  # cylinder surface: A = 2*π*r*l, V = cylinder volume: π*r^2*l, Am = A/V = 2*π*r*l / (π*r^2*l) = 2/r
+  return 2./r
+  #return Am
+
+def get_cm(fiber_no, mu_no):
+  return motor_units[mu_no % len(motor_units)]["cm"]
+  
+def get_conductivity(fiber_no, mu_no):
+  return Conductivity
+
+def get_specific_states_call_frequency(fiber_no, mu_no):
+  stimulation_frequency = motor_units[mu_no % len(motor_units)]["stimulation_frequency"]
+  return stimulation_frequency*1e-3
+
+def get_specific_states_frequency_jitter(fiber_no, mu_no):
+  #return 0
+  return motor_units[mu_no % len(motor_units)]["jitter"]
+
+def get_specific_states_call_enable_begin(fiber_no, mu_no):
+  #return 1
+  return motor_units[mu_no % len(motor_units)]["activation_start_time"]*1e3

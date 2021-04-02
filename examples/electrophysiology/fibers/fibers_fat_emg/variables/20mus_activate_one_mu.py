b903168ccf7bc08a1b80eccdb0125f5a026fc857
Benjamin Maier <maier.bn@gmail.com>
2021-04-02 21:14:03 +0200

    Make end time smaller

--- examples/electrophysiology/fibers/fibers_fat_emg/variables/20mus_activate_one_mu.py
+++ examples/electrophysiology/fibers/fibers_fat_emg/variables/20mus_activate_one_mu.py
@@ -65,7 +65,7 @@ for mu_no in range(n_motor_units):
 
 # timing parameters
 # -----------------
-end_time = 1000.0                      # [ms] end time of the simulation
+end_time = 100.0                      # [ms] end time of the simulation
 stimulation_frequency = 100*1e-3    # [ms^-1] sampling frequency of stimuli in firing_times_file, in stimulations per ms, number before 1e-3 factor is in Hertz.
 stimulation_frequency_jitter = 0    # [-] jitter in percent of the frequency, added and substracted to the stimulation_frequency after each stimulation
 dt_0D = 2.5e-3                        # [ms] timestep width of ODEs (2e-3)

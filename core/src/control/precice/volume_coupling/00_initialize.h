58266316fa236bd3bc607cce5d4ffa06fef5add3
Benjamin Maier <maierbn@gmail.com>
2021-04-10 00:31:35 +0200

    Correctly handle meshes with array indices (e.g., multiple fibers) in precice adapter

--- core/src/control/precice/volume_coupling/00_initialize.h
+++ core/src/control/precice/volume_coupling/00_initialize.h
@@ -41,6 +41,7 @@ protected:
     std::vector<dof_no_t> dofNosLocal;        //< the local dof nos in the 3D mesh of the surface mesh nodes
     std::vector<double> geometryValues;       //< the geometry values, i.e., node positions
     int nNodesLocal;                          //< local number of nodes
+    std::string opendihuMeshName;             //< opendihu mesh name corresponding to the precice mesh
   };
 
   /** one field variable to be transferred, called `data:vector` or `data:scalar` in the precice config

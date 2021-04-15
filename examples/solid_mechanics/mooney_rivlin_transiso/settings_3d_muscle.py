a816ccdafde7bf35c076f12f7ce3a1e71418b5b9
Benjamin Maier <maier.bn@gmail.com>
2021-04-15 12:15:20 +0200

    Improve settings scripts for mooney_rivlin_transiso example

--- examples/solid_mechanics/mooney_rivlin_transiso/settings_3d_muscle.py
+++ examples/solid_mechanics/mooney_rivlin_transiso/settings_3d_muscle.py
@@ -8,18 +8,37 @@ sys.path.insert(0, '..')
 import variables              # file variables.py, defined default values for all parameters, you can set the parameters there
 from create_partitioned_meshes_for_settings import *   # file create_partitioned_meshes_for_settings with helper functions about own subdomain
 
+own_rank_no = (int)(sys.argv[-2])
+n_ranks = (int)(sys.argv[-1])
+
+# define command line arguments
+mbool = lambda x:bool(distutils.util.strtobool(x))   # function to parse bool arguments
+parser = argparse.ArgumentParser(description='3d_hyperelasticity')
+parser.add_argument('--scenario_name',         help='The name to identify this run in the log.',      default="3d_box")
+parser.add_argument('--n_subdomains', nargs=3, help='Number of subdomains in x,y,z direction.',             type=int)
+
+# parse command line arguments and assign values to variables module
+args, other_args = parser.parse_known_args(args=sys.argv[:-2], namespace=variables)
+if len(other_args) != 0 and own_rank_no == 0:
+    print("Warning: These arguments were not parsed by the settings python file\n  " + "\n  ".join(other_args), file=sys.stderr)
+
 
 # input mesh file
 fiber_file = "../../../electrophysiology/input/left_biceps_brachii_13x13fibers.bin"
 
 load_fiber_data = True             # If the fiber geometry data should be loaded completely in the python script. If True, this reads the binary file and assigns the node positions in the config. If False, the C++ code will read the binary file and only extract the local node positions. This is more performant for highly parallel runs.
 
+
+# initialize some dependend variables
+if variables.n_subdomains is not None:
+  variables.n_subdomains_x = variables.n_subdomains[0]
+  variables.n_subdomains_y = variables.n_subdomains[1]
+  variables.n_subdomains_z = variables.n_subdomains[2]
+  
+variables.n_subdomains = variables.n_subdomains_x*variables.n_subdomains_y*variables.n_subdomains_z
+
 # partitioning
 # ------------
-# this has to match the total number of processes
-n_subdomains_x = 1
-n_subdomains_y = 1
-n_subdomains_z = 1
 
 # stride for sampling the 3D elements from the fiber data
 # here any number is possible
@@ -33,7 +52,7 @@ sampling_stride_z = 95
 
 # create the partitioning using the script in create_partitioned_meshes_for_settings.py
 result = create_partitioned_meshes_for_settings(
-    n_subdomains_x, n_subdomains_y, n_subdomains_z, 
+    variables.n_subdomains_x, variables.n_subdomains_y, variables.n_subdomains_z, 
     fiber_file, load_fiber_data,
     sampling_stride_x, sampling_stride_y, sampling_stride_z, generate_linear_3d_mesh=True, generate_quadratic_3d_mesh=True)
 
@@ -134,8 +153,9 @@ config = {
     "loadFactorGiveUpThreshold": 0.1,   # if the adaptive time stepping produces a load factor smaller than this value, the solution will be accepted for the current timestep, even if it did not converge fully to the tolerance
     
     #"loadFactors":  [0.1, 0.2, 0.35, 0.5, 1.0],   # load factors for every timestep
-    "loadFactors": [],                 # no load factors, solve problem directly
-    "nNonlinearSolveCalls": 1,         # how often the nonlinear solve should be repeated
+    "loadFactors": [],                  # no load factors, solve problem directly
+    "scaleInitialGuess":          False,# when load stepping is used, scale initial guess between load steps a and b by sqrt(a*b)/a. This potentially reduces the number of iterations per load step (but not always).
+    "nNonlinearSolveCalls": 1,          # how often the nonlinear solve should be repeated
     
     # boundary conditions
     "dirichletBoundaryConditions": elasticity_dirichlet_bc,
@@ -145,22 +165,22 @@ config = {
     "updateDirichletBoundaryConditionsFunction": None,
     "updateDirichletBoundaryConditionsFunctionCallInterval": 1,
     
-    "dirichletOutputFilename":     "out/dirichlet_boundary_conditions",    # filename for a vtp file that contains the Dirichlet boundary condition nodes and their values, set to None to disable
+    "dirichletOutputFilename":     "out/" + variables.scenario_name + "/dirichlet_boundary_conditions",    # filename for a vtp file that contains the Dirichlet boundary condition nodes and their values, set to None to disable
       
     "OutputWriter" : [   # output files for displacements function space (quadratic elements)
-      {"format": "Paraview", "outputInterval": 1, "filename": "out/u", "binary": False, "fixedFormat": False, "onlyNodalValues":True, "combineFiles":True, "fileNumbering": "incremental"},
-      {"format": "PythonFile", "filename": "out/u", "outputInterval": 1, "binary":False, "onlyNodalValues":True, "fileNumbering": "incremental"},
+      {"format": "Paraview", "outputInterval": 1, "filename": "out/" + variables.scenario_name + "/u", "binary": False, "fixedFormat": False, "onlyNodalValues":True, "combineFiles":True, "fileNumbering": "incremental"},
+      {"format": "PythonFile", "filename": "out/" + variables.scenario_name + "/u", "outputInterval": 1, "binary":False, "onlyNodalValues":True, "fileNumbering": "incremental"},
     ],
     "pressure": {   # output files for pressure function space (linear elements)
       "OutputWriter" : [
-        {"format": "Paraview", "outputInterval": 1, "filename": "out/p", "binary": False, "fixedFormat": False, "onlyNodalValues":True, "combineFiles":True, "fileNumbering": "incremental"},
-        {"format": "PythonFile", "filename": "out/p", "outputInterval": 1, "binary":False, "onlyNodalValues":True, "fileNumbering": "incremental"},
+        {"format": "Paraview", "outputInterval": 1, "filename": "out/" + variables.scenario_name + "/p", "binary": False, "fixedFormat": False, "onlyNodalValues":True, "combineFiles":True, "fileNumbering": "incremental"},
+        {"format": "PythonFile", "filename": "out/" + variables.scenario_name + "/p", "outputInterval": 1, "binary":False, "onlyNodalValues":True, "fileNumbering": "incremental"},
       ]
     },
     # output writer for debugging, outputs files after each load increment, the geometry is not changed but u and v are written
     "LoadIncrements": {   
       "OutputWriter" : [
-        {"format": "Paraview", "outputInterval": 1, "filename": "out/load_increments", "binary": False, "fixedFormat": False, "onlyNodalValues":True, "combineFiles":True, "fileNumbering": "incremental"},
+        {"format": "Paraview", "outputInterval": 1, "filename": "out/" + variables.scenario_name + "/load_increments", "binary": False, "fixedFormat": False, "onlyNodalValues":True, "combineFiles":True, "fileNumbering": "incremental"},
       ]
     },
   },

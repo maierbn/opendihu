d326da4d8f14a35b07831bb8dd8c608962a808e3
Benjamin Maier <maier.bn@gmail.com>
2021-04-20 20:31:28 +0200

    Update plot scripts for solid mechanics validation tests

--- examples/solid_mechanics/shear_test/plot_validation.py
+++ examples/solid_mechanics/shear_test/plot_validation.py
@@ -11,23 +11,34 @@ import matplotlib.pyplot as plt
 import csv
 
 # parse all data from result.csv
-data = {}
-with open('build_release/result.csv') as csvfile:
+a = []
+with open('build_release/result.csv.backup') as csvfile:
   reader = csv.reader(csvfile, delimiter=',')
   
-  for scenario_name,strain,stress in reader:
+  for scenario_name,strain,stress0,stress1,stress2,stress3,stress4,stress5 in reader:
     strain = float(strain)
-    stress = float(stress)
+    stress0 = float(stress0)
+    stress1 = float(stress1)
+    stress2 = float(stress2)
+    stress3 = float(stress3)
+    stress4 = float(stress4)
+    stress5 = float(stress5)
     
     # if value is invalid
-    if abs(strain) > 4 or abs(stress) > 1000:
+    if abs(strain) > 4:
       continue
     
-    if scenario_name not in data:
-      data[scenario_name] = {"strain": [], "stress": []}
-    data[scenario_name]["strain"].append(strain)
-    data[scenario_name]["stress"].append(stress)
+    a.append((scenario_name,strain,stress0,stress1,stress2,stress3,stress4,stress5))
     
+# sort entries
+a = sorted(a)
+data = {}
+for scenario_name,strain,stress0,stress1,stress2,stress3,stress4,stress5 in a:
+  if scenario_name not in data:
+    data[scenario_name] = {"strain": [], "stress": []}
+  data[scenario_name]["strain"].append(strain)
+  data[scenario_name]["stress"].append([stress0,stress1,stress2,stress3,stress4,stress5])
+
 # loop over scenario names
 labels = [
   "Fully incompressible (OpenDiHu)",
@@ -50,10 +61,23 @@ plt.rcParams['lines.linewidth'] = 2
 plt.rcParams['lines.markersize'] = 10
 
 fig, ax = plt.subplots(figsize=(12,6))
+colors = []
 
-for i,scenario_name in enumerate(scenarios):
+labels2 = ["$S_{11}$", "$S_{22}$", "$S_{33}$", "$S_{12}$", "$S_{23}$", "$S_{13}$"]
+for component_no in range(6):
   
-  ax.plot(data[scenario_name]["strain"], data[scenario_name]["stress"], markers[i], markerfacecolor='none', markeredgewidth=2, label=labels[i])
+  scenario_name = "incompressible_mooney_rivlin"
+  i = 0
+  line, = ax.plot(data[scenario_name]["strain"], [stress[component_no] for stress in data[scenario_name]["stress"]], "-", 
+     markerfacecolor='none', markeredgewidth=2, label=labels2[component_no])
+  
+  scenario_name = "nearly_incompressible_mooney_rivlin_febio"
+  i = 1
+  ax.plot(data[scenario_name]["strain"], [stress[component_no] for stress in data[scenario_name]["stress"]], ":", 
+     color=line.get_color(), markerfacecolor='none', markeredgewidth=2)
+  
+ax.plot([0,0],[0,0],"k-",label="OpenDiHu")
+ax.plot([0,0],[0,0],"k:",label="FEBio")
   
 plt.ylabel('stress [$N/cm^2$]')
 plt.xlabel('strain [-]')
--- examples/solid_mechanics/tensile_test/plot_validation.py
+++ examples/solid_mechanics/tensile_test/plot_validation.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import csv
 
 # parse all data from result.csv
-data = {}
+a = []
 with open('build_release/result.csv') as csvfile:
   reader = csv.reader(csvfile, delimiter=',')
   
@@ -22,12 +22,18 @@ with open('build_release/result.csv') as csvfile:
     # if value is invalid
     if abs(strain) > 4 or abs(stress) > 1000:
       continue
-    
-    if scenario_name not in data:
-      data[scenario_name] = {"strain": [], "stress": []}
-    data[scenario_name]["strain"].append(strain)
-    data[scenario_name]["stress"].append(stress)
-    
+      
+    a.append((scenario_name,strain,stress))
+
+# sort entries
+a = sorted(a)
+data = {}
+for scenario_name,strain,stress in a:
+  if scenario_name not in data:
+    data[scenario_name] = {"strain": [], "stress": []}
+  data[scenario_name]["strain"].append(strain)
+  data[scenario_name]["stress"].append(stress)
+
 # loop over scenario names
 labels = [
   "Fully incompressible (OpenDiHu)",

136f0a1db699ad893a01158f54d9409f6f70ea08
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2023-05-19 13:57:50 +0200

    Added checks for libeigen

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -5,6 +5,12 @@ find_package(MPI COMPONENTS C CXX REQUIRED)
 # Check for Python3
 find_package(Python3 COMPONENTS Interpreter Development NumPy REQUIRED)
 
+# Check for Eigen3
+find_package(Eigen3 REQUIRED NO_MODULE)
+if(${Eigen3_FOUND})
+  message(STATUS "Found Eigen3: yes")
+endif()
+
 include_directories(${MPI_CXX_INCLUDE_DIRS}
                     ${Python3_INCLUDE_DIRS}
                    )

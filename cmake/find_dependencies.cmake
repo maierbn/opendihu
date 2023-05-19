6b8203f46098cc8bdeeb89fe2523fb835f54c15b
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2023-05-19 13:33:06 +0200

    Testing cmake for finding numpy installation.

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -3,7 +3,7 @@
 find_package(MPI COMPONENTS C CXX REQUIRED)
 
 # Check for Python3
-find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
+find_package(Python3 COMPONENTS Interpreter Development NumPy REQUIRED)
 
 include_directories(${MPI_CXX_INCLUDE_DIRS}
                     ${Python3_INCLUDE_DIRS}
